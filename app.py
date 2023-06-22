aca776a2490c410b19a69ac260ecd2b28a426b4f
VamsiKrishna <vamsikrishnakandala56@gmail.com>
2023-06-22 20:53:15 +0530

    'a'

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ app = Flask(__name__)
 app.secret_key =secret_key
 
 
-mydb=mysql.connector.connect(host="localhost",user="root",password="vamsi",db="ocp")
-cursor=mydb.cursor()
-'''
+#mydb=mysql.connector.connect(host="localhost",user="root",password="vamsi",db="ocp")
+#cursor=mydb.cursor()
+
 user=os.environ.get('RDS_USERNAME')
 db=os.environ.get('RDS_DB_NAME')
 password=os.environ.get('RDS_PASSWORD')
@@ -24,10 +24,9 @@ with mysql.connector.connect(host=host,user=user,password=password,port=port,db=
     cursor=conn.cursor(buffered=True)
     cursor.execute("create table if not exists usercomp(complaintno varchar(10),issue varchar(1000),description varchar(2000),usermail varchar(100),response varchar(20))")
     cursor.execute("create table if not exists userdata(name varchar(50),email varchar(100),dob varchar(20),password varchar(30))")
-    cursor.execute("create table if not exists adcomp(username varchar(100),password varchar(30))")
     cursor.close()
 mydb=mysql.connector.connect(host=host,user=user,password=password,db=db)
-cursor=mydb.cursor(buffered=True)'''
+cursor=mydb.cursor(buffered=True)
 
 
 
